2140de5805939ab5a432d73a421a1ace56053b49
Hassel2 <hasselprassel@mail.ru>
2023-05-27 21:11:57 +0300

    feat(config.py): move constants from ml_git_cat.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 MODEL = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-6_H-128_A-2/2'
 MODEL_NAME = MODEL.replace('https://tfhub.dev/tensorflow/', '')
 SAVED_MODEL_PATH = './saved_git_model/{}_bert'.format(MODEL_NAME.replace('/', '_'))
+SCALE_RATIO = 2.5 * 100000
+CHUNK_SIZE = 100 * 3
