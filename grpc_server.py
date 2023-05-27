b95372f2ba3bc3b4650020270f8e55bdb0116033
Hassel2 <hasselprassel@mail.ru>
2023-05-27 20:20:14 +0300

    !feat(grpc_server.py): move code from ml_git_cat.py

--- grpc_server.py
+++ grpc_server.py
@@ -13,11 +13,51 @@ import tensorflow_text as text
 from official.nlp import optimization  # to create AdamW optimizer
 import numpy as np
 
+    
+regression_model = tf.saved_model.load(config.SAVED_MODEL_PATH)
 
 class ModelService(msGrpc.ModelServicer):
     def GetGrades(self, request, context):
         link = request.repLink
+        # The sctipt returns path to diffs folder
+        output = subprocess.run(['bash', './analyze.sh', link], capture_output=True, text=True)
+        # Need a substring because the outupt have a \n at line end
+        work_dir = output.stdout[:-1] 
+        diffs_by_author = {}
+
+        try:
+            auhors_dirs = os.listdir(work_dir)
+
+            # build path to found directories
+            auhors_dirs = [os.path.join(work_dir, p) for p in auhors_dirs]
+            for d in auhors_dirs:
+                with open(os.path.join(d, 'author.email')) as e:
+                    email = e.readline()
+                with open(os.path.join(d, 'history')) as h:
+                    history = ''.join(h.readlines())    # read all file
+                diffs_by_author[email] = history
+        except FileNotFoundError as e:
+            print('no diffs found!:', e)
+        except UnicodeDecodeError as e:
+            print('UnicodeDecodeError: ', e)
+
+        for author in diffs_by_author:
+            testing_data = split_by_chunks(diffs_by_author[author])
+            predictions = regression_model(testing_data)
+            print(f"{author}:\n\tPrediction salary =  ~", np.mean(predictions) * SCALE_RATIO)
+            del predictions
+            del diffs_by_author[author]
+
         return super().GetGrades(request, context)
+
+
+def split_by_chunks(data: str):
+    X = []
+    for text in data:
+        chunks = [text[i:i+CHUNK_SIZE] for i in range(0, len(text), CHUNK_SIZE)]
+        if chunks:
+            X += chunks
+    return X
 
 
 def serve():
@@ -29,7 +69,5 @@ def serve():
 
 
 if __name__ == '__main__':
-    regression_model = tf.saved_model.load(config.SAVED_MODEL_PATH)
-    print('loading model: done')
     serve()
 
